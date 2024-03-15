e6fb186d66a1f32affbb88a679f1010ef07e3dc8
zaydahmed04 <zaydahmed04@gmail.com>
2024-03-15 10:20:50 +0000

    Conflict in new branch

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String [] args){
-		System.out.println("Hello World how are you!");
+		System.out.println("Hello World how are you today!");
 	}
 }
